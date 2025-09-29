863ee2849c9dc4e1d8a6cda2b1ff702d7439ac5b
Mihail Hristov <mihail.hristov@intellias.com>
2025-09-29 15:31:35 +0300

    refactor: create variable for hardcoded URL

--- src/database/population.ts
+++ src/database/population.ts
@@ -1,6 +1,8 @@
+const PRODUCT_API_URL = 'https://fakestoreapi.com/products'
+
 export const fetchProducts = async () => {
     try {
-        const response = await fetch('https://fakestoreapi.com/products')
+        const response = await fetch(PRODUCT_API_URL)
         const data = await response.json()
         console.log(data)
         return data
