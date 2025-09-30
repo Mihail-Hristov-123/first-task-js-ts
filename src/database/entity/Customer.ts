aa085ceb28fb71c12fc5299d22553df63710ba45
Mihail Hristov <mihail.hristov@intellias.com>
2025-09-30 11:41:25 +0300

    feat: implement product purchasing

--- src/database/entity/Customer.ts
+++ src/database/entity/Customer.ts
@@ -19,27 +19,34 @@ abstract class Customer {
     private cart: Omit<Product, 'qunatityInStock'>[]
 
     async buyProduct(productId: number) {
+        // Decreases the quantity of a particular product
         try {
             const product = await repository.product.findOneBy({
                 id: productId,
             })
+
             if (!product) {
                 throw new Error('No product was found with the particular ID')
             }
-            if (product.quantityInStock === 0) {
+            const currentQunatity = product?.quantityInStock
+            if (currentQunatity === 0) {
                 console.log(
-                    'Currently this product is unavailable. You will be notified when it is restocked',
+                    'Currently this product is unavailable. You will be notified once it is restocked',
                 )
                 return
             }
-            if (product.quantityInStock < 10 && !this.hasPriority) {
+            if (currentQunatity < 10 && !this.hasPriority) {
                 console.log(
-                    `As there are only ${product.quantityInStock} articles of this type left, they are reserved for our premium customers`,
+                    `As there are only ${currentQunatity} articles of this type left, they are reserved for our premium customers`,
                 )
                 return
             }
-            // to be continued
-        } catch (error) {}
+            product.quantityInStock = currentQunatity - 1
+            await repository.product.save(product)
+            console.log(`${product.name} purchased successfully`)
+        } catch (error) {
+            console.error(`An error occurred during the purchase: ${error}`)
+        }
     }
 
     public constructor(
@@ -50,6 +57,7 @@ abstract class Customer {
         this.name = name
         this.hasDiscounts = hasDiscounts
         this.hasPriority = hasPriority
+        this.cart = []
     }
 }
 
